79912b34f2db38c805b8b75bb187fd39bb80ccb2
Robinpreet kaur <Rrobinpreetkaur30@rrc.ca>
2026-01-23 12:16:56 -0600

    Completed the installation of email-validator.

---
+++ tests/test_bank_account.py
@@ -0,0 +1,7 @@
+"""Unit testing for the BankAccount class.
+
+Usage: 
+
+To execute all tests in the terminal execute the following command:
+    python -m unittest tests/test_bank_account.py
+"""
---
+++ tests/test_client.py
@@ -0,0 +1,7 @@
+"""Unit testing for the Client class.
+
+Usage: 
+
+To execute all tests in the terminal execute the following command:
+    python -m unittest tests/test_client.py
+"""
