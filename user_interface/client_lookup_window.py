513cd734b6a3950445715dfdc11dd2374a082a78
Robinpreet kaur <Rrobinpreetkaur30@rrc.ca>
2026-04-02 17:18:47 -0500

    adding on lookup client method slot for lookup button clicked signal

--- user_interface/client_lookup_window.py
+++ user_interface/client_lookup_window.py
@@ -26,3 +26,54 @@ class ClientLookupWindow(LookupWindow):
         self.lookup_button.clicked.connect(self.__on_lookup_client)
         self.client_number_edit.textChanged.connect(self.__on_text_changed)
         self.account_table.cellClicked.connect(self.__on_select_account)  
+
+    @Slot()
+    def __on_lookup_client(self):
+        """Handling lookup button clicked."""
+        client_number_text = self.client_number_edit.text().strip()
+
+        try:
+            client_number = int(client_number_text)
+        except ValueError:
+            self.reset_display()
+            QMessageBox.warning(self,"Non-Numeric Client","Client number must be a numeric value.")
+            return
+        
+        if client_number not in self.client_listing:
+            self.reset_display()
+            QMessageBox.information(self,"Client Not Found",
+                f"Client with number: {client_number} not found.")
+            return
+        
+        client = self.client_listing[client_number]
+        client_name = f"{client.first_name} {client.last_name}"
+        self.client_info_label.setText(client_name)
+        self.account_table.setRowCount(0)
+
+        row = 0
+        for account in self.accounts.values():
+
+            if account.client_number == client_number:
+
+                self.account_table.insertRow(row)
+
+                item_number = QTableWidgetItem(str(account.account_number))
+                item_number.setTextAlignment(Qt.AlignCenter)
+                self.account_table.setItem(row, 0, item_number)
+
+                item_balance = QTableWidgetItem(f"${account.balance:,.2f}")
+                item_balance.setTextAlignment(Qt.AlignRight | Qt.AlignVCenter)
+                self.account_table.setItem(row, 1, item_balance)
+
+                item_date = QTableWidgetItem(str(account.date_created))
+                item_date.setTextAlignment(Qt.AlignCenter)
+                self.account_table.setItem(row, 2, item_date)
+
+                item_type = QTableWidgetItem(account.__class__.__name__)
+                item_type.setTextAlignment(Qt.AlignCenter)
+                self.account_table.setItem(row, 3, item_type)
+
+                row += 1
+
+        self.account_table.resizeColumnsToContents()
+
