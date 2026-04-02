43d5e23c8385ba3984bb07686e5f8ed2a54c236c
Robinpreet kaur <Rrobinpreetkaur30@rrc.ca>
2026-04-02 17:13:05 -0500

    updating clientwindow file by adding super class init method by deleting pass statement

--- user_interface/client_lookup_window.py
+++ user_interface/client_lookup_window.py
@@ -16,5 +16,13 @@ __credits__ = "<your name here>"
 
 class ClientLookupWindow(LookupWindow):
     """Represents a main window for looking up BankAccounts.""" 
+    def __init__(self):
+        super().__init__()
 
-    pass        
+        clients, accounts = load_data()
+        self.client_listing = clients
+        self.accounts = accounts
+
+        self.lookup_button.clicked.connect(self.__on_lookup_client)
+        self.client_number_edit.textChanged.connect(self.__on_text_changed)
+        self.account_table.cellClicked.connect(self.__on_select_account)  
